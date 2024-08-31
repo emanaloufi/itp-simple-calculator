26b5c8f6eb73190ac1b7f4de810c538cdd84ac5e
emanaloufi <129239249+emanaloufi@users.noreply.github.com>
2024-08-31 20:43:47 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,26 @@
-def add(x, y):
-    return x + y
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Invalid value for denominator, can't divide by 0!"
+    return a / b
 
 
-def subtract(x, y):
-    pass
+def square(a):
+    return a * a
 
+def power(a, b):
+    return a ** b
 
-def divide(x, y):
-    pass
-
-
-def multiply(x, y):
-    pass
-
-
-def square(x):
-    pass
-
-
-def power(x, y):
-    pass
-
-
-def sqrt(x):
-    pass
+def sqrt(a):
+    if a < 0:
+        return "Invalid value for square root, can't take the square root of a negative number!"
+    return a ** 0.5
